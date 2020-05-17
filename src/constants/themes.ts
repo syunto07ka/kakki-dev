5a5a73335e4ad710098c732ce738a0348323bf67
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-05-17 11:38:18 +0900

    feat: make getTheme selector

---
+++ src/constants/themes.ts
@@ -0,0 +1,11 @@
+export const whiteTheme = {
+  background: '#fff',
+  base: '#333333',
+  main: '#f06060'
+}
+
+export const blackTheme = {
+  background: '#333333',
+  base: '#fff',
+  main: '#f06060'
+}
