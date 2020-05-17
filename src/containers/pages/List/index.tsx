4a35e25266e224a312694919c3087cb68e369568
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-05-17 10:20:17 +0900

    feat: use articles state

--- src/containers/pages/List/index.tsx
+++ src/containers/pages/List/index.tsx
@@ -1,7 +1,25 @@
 import * as React from 'react';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
+import { articles as articlesAtom } from 'states/atoms/articles';
+import { getArticles } from 'states/selectors/getArticles';
+import { Article } from "model";
 
-export const List: React.FC = () => (
-  <div>
-    List Component
-  </div>
-);
+export const List: React.FC = () => {
+  const setArticles = useSetRecoilState(articlesAtom);
+  const articles: any = useRecoilValue(getArticles);
+
+  React.useEffect(() => {
+    setArticles([]);
+  });
+
+  return (
+    <div>
+      {articles.map((article: Article) => (
+        <div key={String(article.id)}>
+          <div>{article.id}</div>
+          <div>{article.title}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
