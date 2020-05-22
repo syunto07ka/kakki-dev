3af5d3b90f0753661ad106c81866d90cbed162b2
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-05-23 03:59:54 +0900

    refactor: exchange Navigation from styled-component to css-modules

--- src/components/molecules/Navigation/index.tsx
+++ src/components/molecules/Navigation/index.tsx
@@ -1,30 +1,13 @@
 import * as React from 'react';
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import './style.module.scss';
 
 interface Props {
   isActive: boolean
 }
 
 export const Navigation: React.FC<Props> = () => (
-  <Wrapper>
-    <Nav to="/">Blog</Nav>
-  </Wrapper>
+  <div styleName="wrapper">
+    <Link styleName="nav" to="/">Blog</Link>
+  </div>
 );
-
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 24px;
-`;
-
-const Nav = styled(Link)`
-  width: 160px;
-  padding: 8px 16px;
-  text-align: center;
-  color: #333333;
-  text-decoration: none;
-  border-bottom: 1px solid #d9d9d9;
-`;
-
-// opacity: ${() => props.isActive ? 1 : 0.7};
