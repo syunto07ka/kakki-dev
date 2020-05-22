f9e2e02b827e5a898a28cd9815e90c8b3fd829d5
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-05-23 03:06:35 +0900

    fix: fix title style

--- src/components/organizms/Header/index.tsx
+++ src/components/organizms/Header/index.tsx
@@ -19,9 +19,11 @@ const Wrapper = styled.div`
   margin-top: 32px;
 `;
 
-const Title = styled(Link)(props => `
-  color: ${props.theme.base};
+const Title = styled(Link)`
+  color: #333333;
   text-align: center;
   font-size: 48px;
   font-weight: bold;
-`);
+  display: block;
+  text-decoration: none;
+`;
