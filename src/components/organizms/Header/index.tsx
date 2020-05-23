609711cb7626632a62cfb0abfd59fc69d463224f
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-05-23 13:34:38 +0900

    refactor: exchange Header from styled-component to css-modules

--- src/components/organizms/Header/index.tsx
+++ src/components/organizms/Header/index.tsx
@@ -1,29 +1,16 @@
 import * as React from 'react';
-import styled from 'styled-components';
 import { Theme } from 'model';
 import { Navigation } from 'components/molecules/Navigation';
 import { Link } from 'react-router-dom';
+import './style.module.scss';
 
 interface Props {
   theme: Theme
 }
 
 export const Header: React.FC<Props> = () => (
-  <Wrapper>
-    <Title to="/">kakki-dev</Title>
+  <div styleName="wrapper">
+    <Link styleName="title" to="/">kakki-dev</Link>
     <Navigation isActive={true}/>
-  </Wrapper>
+  </div>
 );
-
-const Wrapper = styled.div`
-  margin-top: 32px;
-`;
-
-const Title = styled(Link)`
-  color: #333333;
-  text-align: center;
-  font-size: 48px;
-  font-weight: bold;
-  display: block;
-  text-decoration: none;
-`;
