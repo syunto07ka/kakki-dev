7617ae684ba2e31c73159b57926b7304b0c4d4b6
垣井駿努 <syunto07ka@yahoo.co.jp>
2020-06-08 00:22:14 +0900

    feat: add blog

--- src/constants/articles.ts
+++ src/constants/articles.ts
@@ -2,24 +2,31 @@ import { Article } from "model";
 import { Start } from "constants/blogs/Start";
 import AddImplementes from "constants/blogs/AddImplementes.mdx";
 import Moteamasu from "constants/blogs/Moteamasu.mdx";
+import Saddness from "constants/blogs/Sadness.mdx";
 
 export const articles: Article[] = [
   {
+    id: 4,
+    title: 'モテアマスから帰ってきて',
+    date: '2020-06-08',
+    description: Saddness,
+  },
+  {
     id: 3,
     title: 'モテアマス三軒茶屋に来ました',
-    date: '2020年6月3日',
+    date: '2020-06-03',
     description: Moteamasu,
   },
   {
     id: 2,
     title: 'ちょっと改造しました',
-    date: '2020年5月26日',
+    date: '2020-05-26',
     description: AddImplementes,
   },
   {
     id: 1,
     title: '個人ブログ始めてみました',
-    date: '2020年5月17日',
+    date: '2020-05-17',
     description: Start,
   }
 ];
